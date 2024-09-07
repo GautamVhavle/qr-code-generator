d075ea54ec7c62e5c5e0cca5d6ffe33f9dc3a400
GautamVhavle <gautamvhavle@gmail.com>
2024-09-07 19:36:29 +0530

    initial commit: setup foundation files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './styles/App.scss';
+import { useState } from 'react';
+
+
+
+export default function App() {
+
+  return (
+      <div>
+        <h1>Welcome</h1>
+      </div>
+  );
+}
---
+++ src/components/Tool.js
@@ -0,0 +1,12 @@
+import '../styles/components/tool.scss';
+
+export default function Tool() {
+
+    return (
+        <div>
+            <h1>
+                Welcome to qr code generator
+            </h1>
+        </div>
+    );
+}
